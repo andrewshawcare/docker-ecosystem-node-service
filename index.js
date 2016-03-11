0e9f50e1672416b5352eb99dee7437526d936d43
Your Name <your@email.address>
2016-03-11 10:58:04 -0500

    Delegate port assignment; add error handling

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const http = require("http");
 const pg = require("pg");
-const hostname = "0.0.0.0";
+const hostname = "127.0.0.1";
 const port = 5678;
 
 http.createServer((request, response) => {
   pg.connect("postgres://postgres:@database/postgres", (error, client, done) => {
-    client.query("select id, name from person", (error, result) => {
+    if (error) {
       done();
-      response.writeHead(200, {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Request-Method": "*",
-        "Access-Control-Allow-Headers": "*"
-      });
+      console.log(error);
+      response.writeHead(500);
+      response.end();
+    } else {
+      client.query("select id, name from person", (error, result) => {
+        done();
+        response.writeHead(200, {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Request-Method": "*",
+          "Access-Control-Allow-Headers": "*"
+        });
 
-      response.end(JSON.stringify(result.rows));
-    });
+        response.end(JSON.stringify(result.rows));
+      });
+    }
   });
 }).listen(port, hostname, () => {
   console.log(`Server listening at http://${hostname}:${port}`);
